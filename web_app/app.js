b1e3e916845a5881ff36e0d720555d3eab789ddc
sekiro-1161 <167535621+sekiro-1161@users.noreply.github.com>
2026-03-13 18:09:23 +0300

    Add files via upload

---
+++ web_app/app.js
@@ -0,0 +1,6 @@
+// Инициализация Telegram Web App
+const tg = window.Telegram.WebApp;
+tg.expand(); // Разворачиваем на весь экран
+tg.enableClosingConfirmation(); // Подтверждение при закрытии
+
+// Данные о бург
