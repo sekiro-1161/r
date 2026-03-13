e140bd1d3bbaf226bdd308942aee9a05fd642d73
sekiro-1161 <167535621+sekiro-1161@users.noreply.github.com>
2026-03-14 02:38:00 +0300

    Update app.js

--- web_app/app.js
+++ web_app/app.js
@@ -1,9 +1,3 @@
-// Инициализация Telegram Web App
-const tg = window.Telegram.WebApp;
-tg.expand(); // Разворачиваем на весь экран
-tg.enableClosingConfirmation(); // Подтверждение при закрытии
-
-// Данные о бург
 // Глобальные переменные
 let tg;
 let cart = [];
